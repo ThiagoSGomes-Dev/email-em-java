1a5a314fafb894558e9be705df359fd0c63e28f8
ThiagoGs-Dev <96384024+ThiagoGs-Dev@users.noreply.github.com>
2024-03-10 17:44:41 -0300

    feat(Email): adicionar construtor parametrizado

--- src/Email.java
+++ src/Email.java
@@ -1,10 +1,16 @@
 public class Email {
+    private String email;
     private String subject;
     private String body;
 
-    public Email(String subject, String body) {
+    public Email(String email, String subject, String body) {
+        this.email = email;
         this.subject = subject;
         this.body = body;
+    }
+
+    public String getEmail() {
+        return email;
     }
 
     public String getSubject() {
